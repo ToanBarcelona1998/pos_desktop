f7d60f3a870be8375746556af1ba56e00abcd598
ToanNguyen <toannv.work.198@gmail.com>
2025-12-10 11:19:06 +0700

    feat: update print service

--- lib/src/core/services/print_service.dart
+++ lib/src/core/services/print_service.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
-
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:printing/printing.dart';
 /// Print service for printing invoices
 /// Handles invoice generation and printing
@@ -23,42 +20,15 @@ class PrintService {
     required String name,
   }) async {
     try {
-      final Completer<Uint8List?> completer = Completer();
+      Uint8List pdfBytes = await Printing.convertHtml(html: invoiceHtml);
 
-      Uint8List ? pdfBytes;
-      if (Platform.isWindows || Platform.isMacOS) { // [TODO] should be have a webview on page and hidden it.
-        // final InAppWebView webView = InAppWebView(
-        //   initialUrlRequest: URLRequest(
-        //     url: WebUri.uri(
-        //       Uri.dataFromString(
-        //         invoiceHtml,
-        //         mimeType: 'text/html',
-        //         encoding: Utf8Codec(),
-        //       ),
-        //     ),
-        //   ),
-        //   onWebViewCreated: (controller) async{
-        //     pdfBytes = await controller.createPdf();
-        //     completer.complete(pdfBytes);
-        //   },
-        // );
-      } else {
-        pdfBytes = await Printing.convertHtml(html: invoiceHtml);
-
-        completer.complete(pdfBytes);
-      }
-
-      final bytes = await completer.future;
-
-      if(bytes != null){
-        // Print the invoice
-        await Printing.layoutPdf(
-          onLayout: (format) {
-            return bytes;
-          },
-          name: name,
-        );
-      }
+      // Print the invoice
+      await Printing.layoutPdf(
+        onLayout: (format) {
+          return pdfBytes;
+        },
+        name: name,
+      );
     } catch (e) {
       throw Exception('Failed to print invoice: $e');
     }
