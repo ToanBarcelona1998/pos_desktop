9be2bb48eb87837e48d2794c75bcd06ad4b4df4e
ToanNguyen <toannv.work.198@gmail.com>
2025-12-15 10:22:17 +0700

    feat: update logger

--- domain/lib/src/core/logger.dart
+++ domain/lib/src/core/logger.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/foundation.dart';
-import 'dart:developer' as dev;
+
+enum LogType { error, info, success, log }
 
 sealed class Logger{
-  static const String reset = '\x1B[0m';
-  static const String red = '\x1B[31m';
-  static const String yellow = '\x1B[33m';
-  static const String blue = '\x1B[34m';
+  static const _colors = {
+    LogType.error: "\x1B[31m",
+    LogType.info: "\x1B[33m",
+    LogType.success: "\x1B[32m",
+    LogType.log: "\x1B[34m",
+  };
 
-  static void log(String message) {
+  static const String _reset = "\x1B[0m";
+
+  static void _print(LogType type, String message, {String? tag}) {
     if (kDebugMode) {
-      dev.log('$red[LOG]: $message$reset');
+      final time = DateTime.now().toIso8601String();
+      final color = _colors[type] ?? "";
+      final label = type.name.toUpperCase();
+      final tagStr = tag != null ? "[$tag] " : "";
+      print("$color[$label][$time] $tagStr$message$_reset");
     }
   }
 
-  static void logE(String message, [Object ? e ,String ? tag]) {
-    if (kDebugMode) {
-      dev.log('$red[ERROR]-${tag ?? 'NO TAG'}: $message$reset', error: e);
+  static void log(String message) => _print(LogType.log, message);
 
-    }
-  }
+  static void logE(String message, [Object ? e ,String ? tag]) => _print(LogType.error, message, tag: tag);
 
-  static void logI(String message, {String ? tag}) {
-    if (kDebugMode) {
-      dev.log('$yellow[INFO]-${tag ?? 'NO TAG'}: $message$reset');
-    }
-  }
+  static void logI(String message, {String ? tag}) => _print(LogType.info, message, tag: tag);
 
 }
 
