a9d17c642c39211a2bfa6b1e5c94dd1e1c73da77
ToanNguyen <toannv.work.198@gmail.com>
2025-12-16 17:12:05 +0700

    feat: update image cache

--- data/lib/src/utils/image_cache_helper.dart
+++ data/lib/src/utils/image_cache_helper.dart
@@ -25,7 +25,7 @@ class ImageCacheHelper {
       // Get cache directory
       final Directory cacheDir = await getApplicationDocumentsDirectory();
       final Directory imageCacheDir = Directory(
-        path.join(cacheDir.path, 'payment_account_images', cacheSubdirectory),
+        path.join(cacheDir.path, 'offline_images', cacheSubdirectory),
       );
 
       // Create directory if it doesn't exist
@@ -71,7 +71,7 @@ class ImageCacheHelper {
     try {
       final Directory cacheDir = await getApplicationDocumentsDirectory();
       final Directory imageCacheDir = Directory(
-        path.join(cacheDir.path, 'payment_account_images', cacheSubdirectory),
+        path.join(cacheDir.path, 'offline_images', cacheSubdirectory),
       );
 
       if (!await imageCacheDir.exists()) {
