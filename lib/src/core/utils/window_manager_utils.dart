f2e36febefc2ee955381c90d4dc1b5d4ad2c2780
ToanNguyen <toannv.work.198@gmail.com>
2025-12-18 10:36:17 +0700

    feat: add custom window controller

--- lib/src/core/utils/window_manager_utils.dart
+++ lib/src/core/utils/window_manager_utils.dart
@@ -4,6 +4,29 @@ import 'dart:ui';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:window_manager/window_manager.dart';
 
+extension WindowControllerExtension on WindowController {
+  Future<void> customMethods() async {
+    return await setWindowMethodHandler((call) async {
+      switch (call.method) {
+        case 'window_center':
+          return await windowManager.center();
+        case 'window_close':
+          return await windowManager.close();
+        default:
+          return;
+      }
+    });
+  }
+
+  Future<void> center() {
+    return invokeMethod('window_center');
+  }
+
+  Future<void> close() {
+    return invokeMethod('window_close');
+  }
+}
+
 enum WindowType {
   none('none'),
   onlineCustomer('onlineCustomer'),
@@ -83,14 +106,18 @@ sealed class WindowManagerUtils {
     }
   }
 
-  static Future<WindowController> createNewWindow(WindowArguments arguments) {
-    return WindowController.create(
+  static Future<WindowController> createNewWindow(WindowArguments arguments) async{
+    final controller = await WindowController.create(
       WindowConfiguration(
         arguments: jsonEncode(
           arguments.toJson(),
         ),
-        hiddenAtLaunch: false
+        hiddenAtLaunch: false,
       ),
     );
+
+    await controller.center();
+
+    return controller;
   }
 }
